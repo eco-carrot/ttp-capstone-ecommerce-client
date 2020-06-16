1ae0545792f6b87ae3c6b0d404ab17f6d33fa4bb
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-16 18:41:59 -0400

    add navigation bar

---
+++ src/components/views/NavBarView.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBarView = (props) => {
+  return (
+    <nav>
+      <Link to="/" className="nav-link">
+        Home
+      </Link>
+      <Link to="/login" className="nav-link">
+        Login
+      </Link>
+      <Link to="/shoppingCart" className="nav-link">
+        Shopping Cart
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBarView;
