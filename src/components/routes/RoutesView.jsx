d3afcc9f1aa61adb7bdc7bdef61bd1a6d835d928
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-16 19:02:23 -0400

    import shoppingCart and HomeRoute

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import {
   AllItemsContainer,
+  ShoppingCartContainer
 } from "../containers";
 
 const RoutesView = () => {
   return (
     <Switch>            
+      <Route exact path="/" component={AllItemsContainer} />
       <Route exact path="/items" component={AllItemsContainer} />
       <Route exact path="/shoppingCart" component={ShoppingCartContainer} />
     </Switch>
