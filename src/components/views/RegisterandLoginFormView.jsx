5fcf6ad79d4a1ff4cb5256f061ea9c261f04a57b
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-19 17:08:11 -0400

    form view depending on path

--- src/components/views/RegisterandLoginFormView.jsx
+++ src/components/views/RegisterandLoginFormView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const RegisterandLoginFormView = props => {
   const { name, displayName, handleSubmit, error, handleChange, isLoggedIn, userEmail } = props;
@@ -8,6 +9,7 @@ const RegisterandLoginFormView = props => {
       {isLoggedIn ? `The current logged in user is: ${userEmail}` : ""}
       {name==="login"?
         <form onSubmit={handleSubmit} name={name}>
+        <h3>Log in </h3>
         <div>
           <label htmlFor="email">
             <small>Email</small>
@@ -22,11 +24,17 @@ const RegisterandLoginFormView = props => {
         </div>
         <div>
           <button type="submit">{displayName}</button>
+        <div>
+          <Link to={`/signup/`}>
+                <button>Sign Up if you don't have an Account with us yet :D</button>
+          </Link>
+        </div>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
       :
       <form onSubmit={handleSubmit} name={name}>
+        <h3>Sign Up Page</h3>
         <div>
           <label>
             <small>First Name</small>
@@ -53,6 +61,11 @@ const RegisterandLoginFormView = props => {
         </div>
         <div>
           <button type="submit">{displayName}</button>
+        </div>
+        <div>
+          <Link to={`/login/`}>
+                <button>Log In if you have an Account Already :D</button>
+          </Link>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>}
