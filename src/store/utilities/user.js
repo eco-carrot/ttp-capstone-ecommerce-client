c757c3e43a9aeaa59bc185f029ff076333f69c92
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-19 22:44:07 -0400

    logOut user OK

--- src/store/utilities/user.js
+++ src/store/utilities/user.js
@@ -53,7 +53,7 @@ export const auth = (userObj, method) => async dispatch => {
 
 export const logout = () => async dispatch => {
   try {
-    await axios.delete("/auth/logout", { withCredentials: true });
+    await axios.get(`/auth/logout`, { withCredentials: true });
     dispatch(removeUser());
   }
   catch (err) {
